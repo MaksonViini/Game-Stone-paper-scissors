a0b2651ab4ac519c1b5b9b88fe596b7e46cc724f
MaksonViini <51179463+MaksonViini@users.noreply.github.com>
2019-12-23 16:01:12 -0300

    Add files via upload

---
+++ JoKenPoh.py
@@ -0,0 +1,45 @@
+from time import sleep
+from random import randint
+computador=randint(0,2)
+itens=["PEDRA","PAPEL","TESOURA"]
+print("Suas Opcoes: ")
+print("[0] PEDRA")
+print("[1] PAPEL")
+print("[2] TESOURA")
+jogador=int(input("Qual a sua jogada: "))
+print("JO")
+sleep(1)
+print("KEN")
+sleep(1)
+print("POHH!!")
+print("-=-"*15)
+print("computador jogou {} ".upper().format(itens[computador]))
+print("Voce jogou {} ".upper().format(itens[jogador]))
+print("-=-"*15)
+if(computador==0):
+    if(jogador==0):
+        print("EMPATE!!!")
+    elif(jogador==1):
+        print("JOGADOR GANHOU!!!")
+    elif(jogador==2):
+        print("COMPUTADOR GANHOU!!!")
+    else:
+        print("JOGADA INVALIDA!!!")
+elif(computador==1):
+    if(jogador==0):
+        print("COMPUTADOR GANHOU!!!")
+    elif(jogador==1):
+        print("EMPATE!!!")
+    elif(jogador==2):
+        print("JOGADOR GANHOU!!!")
+    else:
+        print("JOGADA INVALIDA!!!")
+elif(computador==2):
+    if(jogador==0):
+        print("JOGADOR GANHOU!!!")
+    elif(jogador==1):
+        print("JCOMPUTADOR GANHOU!!!")
+    elif(jogador==2):
+         print("EMPATE!!!")
+    else:
+        print("JOGADA INVALIDA!!!")    
